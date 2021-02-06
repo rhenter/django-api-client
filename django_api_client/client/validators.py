43f8d6e45839db326455731400e58fe76cc638c7
rafaelhenter <rafael@henter.com.br>
2021-02-06 11:58:27 -0300

    Add a Better message on 500 errors

--- django_api_client/client/validators.py
+++ django_api_client/client/validators.py
@@ -20,7 +20,7 @@ def validate_status_code(status_code: int) -> Union[
     elif status_code in range(status.HTTP_405_METHOD_NOT_ALLOWED, status.HTTP_417_EXPECTATION_FAILED + 1):
         exception_class = APIError(str(status_code))
     elif status.is_server_error(status_code):
-        exception_class = ServerError(str(status_code))
+        exception_class = ServerError(str(status_code), {'errorMessage': 'API experiencing difficulties. Try later.'})
 
     if exception_class:
         return False, exception_class
